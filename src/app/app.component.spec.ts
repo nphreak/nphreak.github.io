272d167b415cc624606847d114e113bec87e1611
nasaphreak <phr.pereira1992@gmail.com>
2020-11-12 22:53:42 -0300

    Primeiro Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nasaphreak-site'`, () => {
+  it(`should have as title 'nasaphreak-source'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nasaphreak-site');
+    expect(app.title).toEqual('nasaphreak-source');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('nasaphreak-site app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('nasaphreak-source app is running!');
   });
 });
