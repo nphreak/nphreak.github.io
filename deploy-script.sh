a92afe48c4d9aa5cf1b86386c6c4b709f88e37d0
Pedro nPhreak Rodrigues <phr.pereira1992@gmail.com>
2021-06-05 15:41:10 -0500

    teste de novos comandos

--- deploy-script.sh
+++ deploy-script.sh
@@ -7,6 +7,8 @@ mkdir docs
 cp -r ./dist/nasaphreak/* ./docs/
 
 git substree split -P docs -b gh-pages
+git subtree add --prefix docs pages master --squash
+git read-tree --prefix=docs -u pages/master
 
 
 git branch gh-pages
