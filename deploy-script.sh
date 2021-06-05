64364fa1ba4d6006c199e1a2db69c1a29bc1fa64
Pedro nPhreak Rodrigues <phr.pereira1992@gmail.com>
2021-06-05 14:53:19 -0500

    (script): criando branch sem checkout

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,7 +1,10 @@
 git add remote pages https://github.com/nasaphreak/nasaphreak.github.io.git
-git checkout deploy
-
-
 
 npm install
 npm run build
+mkdir docs
+
+cp -r ./dist/nasaphreak/* ./docs/
+
+git branch gh-pages
+git filter-branch -f --subdirectory-filter ./docs/ gh-pages
